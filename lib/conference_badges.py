481d0f1847a0875cbe1cf5dd2bc37dcf87b95279
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-12-14 13:39:00 -0500

    final product

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(f'Hello, my name is {name}.')
+    return badges
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    index = 0
+    while index < len(names):
+        room_assignments.append(f"Hello, {names[index]}! You'll be assigned to room {index + 1}!")
+        index += 1
+    return room_assignments
 
 def printer(names):
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+
+    for room_assignment in rooms:
+        print(room_assignment)
     return None
